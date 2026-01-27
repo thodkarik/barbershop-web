990b6607e6903750a0d9d1a5f5804ccc1c31f22c
thodkarik <thod.karik@gmail.com>
2026-01-27 20:20:27 +0200

    feat: add page placeholders for routing

---
+++ src/features/appointments/BookAppointmentPage.tsx
@@ -0,0 +1,3 @@
+export default function BookAppointmentPage() {
+    return <h1>Book Appointment</h1>;
+}
---
+++ src/features/appointments/MyAppointmentsPage.tsx
@@ -0,0 +1,3 @@
+export default function MyAppointmentsPage() {
+    return <h1>My Appointments</h1>;
+}
